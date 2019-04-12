87280d7aa51825aa4977159fae19aa8b4d6ec7c9
Juan Gaspar García <gaxdpar@gmail.com>
2019-04-12 16:39:03 +0200

    Reorganizadas varias seccioines.

--- init.el
+++ init.el
@@ -39,19 +39,8 @@
 (setq ring-bell-function 'ignore)
 ;;(shell-command-to-string "echo -n $(date +%k:%M--%m-%d)")
 
-(use-package rainbow-delimiters
-  :ensure t
-  :init
-  (add-hook 'prog-mode-hook 'rainbow-delimiters-mode 1))
-(use-package telephone-line
-  :ensure t) ;; Bueno, esto necesita un repaso gordo. Espero que mole mucho.
-(setq telephone-line-subseparator-faces '())
-(setq telephone-line-height 24
-      telephone-line-evil-use-short-tag t)
-(telephone-line-mode t)
-
-
 ;; ----- Apariencia. -----
+
 ; --- Cursor. ---
 (setq-default cursor-type 'bar)
 
@@ -61,25 +50,39 @@
   :init (load-theme 'nyx t))
 
 ; --- Fondo. ---
-;(set-background-color "#101010")
+(set-background-color "#101010")
 
 ; --- Fuente. ---
-(add-to-list 'default-frame-alist '(font . "FiraCode-14"))
-(set-face-attribute 'default nil :family "FiraCode-14")
-(set-default-font "FiraCode-14")
+(add-to-list 'default-frame-alist '(font . "FiraCode-12"))
+(set-face-attribute 'default nil :family "FiraCode-12")
+(set-default-font "FiraCode-12")
+(load "~/.emacs.d/fira-code-mode.el") ; Usar fira-code-mode para ligaduras.
 
-; Usar fira-code-mode para ligaduras.
-(load "~/.emacs.d/fira-code-mode.el")
+; ----- Paquetes. -----
 
-; Unas cuantas utilidades para mejorar nuestra experiencia
+
+(use-package rainbow-delimiters
+  :ensure t
+  :init
+  (add-hook 'prog-mode-hook 'rainbow-delimiters-mode 1))
+
+(use-package telephone-line
+  :ensure t) ;; Bueno, esto necesita un repaso gordo. Espero que mole mucho.
+(setq telephone-line-subseparator-faces '())
+(setq telephone-line-height 24
+      telephone-line-evil-use-short-tag t)
+(telephone-line-mode t)
+
 (use-package windmove
   :ensure t
   :bind (("M-<up>" . windmove-up)
 	 ("M-<down>" . windmove-down)
 	 ("M-<right>" . windmove-right)
 	 ("M-<left>" . windmove-left)))
+
 (use-package ido
   :init (ido-mode))
+
 (use-package neotree
   :ensure t
   :bind (("C-x n" . neotree-toggle)))
@@ -89,50 +92,19 @@
   :config (require 'helm-config)
   :bind (("C-x C-f" . helm-find-files)
 	 ("M-x" . helm-M-x)))
+
 (use-package ac-helm
   :ensure t)
+
 ;;(use-package auto-complete
 ;;  :ensure t
 ;;  :config (ac-config-default))
 
-(use-package company
-  :ensure t
-  :init
-  :config (add-hook 'after-init-hook 'global-company-mode))
-
-;; Varios Básicos
-(electric-pair-mode 1)
-(show-paren-mode 1)
-(setq backup-directory-alist '(("" . "~/.emacs.d/emacs-backup")))
-
-;; Juegos :D
-(use-package tetris
-  :ensure t )
-(use-package typing
-  :ensure t )
-(use-package poker
-  :ensure t )
-;; Accesorios varios
-;;(use-package zone-rainbow
-;;  :ensure t
-;;  :bind ("C-z" . zone-rainbow))
-(use-package golden-ratio
-  :ensure t
-  :config (golden-ratio-mode t))
-(use-package rainbow-mode
-  :ensure t
-  :config (rainbow-mode t))
-;; Modos para programar (Aunque no hay nada de programación aquí xD) y demás
-(use-package markdown-mode+
-  :ensure t)
-(use-package flymd
-  :ensure t)
-(use-package magit
-  :ensure t)
 ;; Terminal mejorada
 (use-package multi-term
   :ensure t
   :bind (("C-t" . multi-term-dedicated-toggle)))
+
 ;; Muchos cursores
 (use-package multiple-cursors
   :ensure t
@@ -140,16 +112,61 @@
          ("C->" . mc/mark-next-like-this)
 	 ("C-<" . mc/mark-previous-like-this)
 	 ("C-c C-<" . mc/mark-all-like-this)))
+
+(use-package company
+  :ensure t
+  :init
+  :config (add-hook 'after-init-hook 'global-company-mode))
+
+;; Accesorios varios
+;;(use-package zone-rainbow
+;;  :ensure t
+;;  :bind ("C-z" . zone-rainbow))
+
+(use-package golden-ratio
+  :ensure t
+  :config (golden-ratio-mode t))
+
+(use-package rainbow-mode
+  :ensure t
+  :config (rainbow-mode t))
+
+;; --- Lenguajes. ---
+
+(use-package markdown-mode+
+  :ensure t)
+
+(use-package flymd
+  :ensure t)
+
+(use-package magit
+  :ensure t)
+
 ;(use-package auctex
 ;  :ensure t
 ;  :config ((setq TeX-auto-save t)
 ;	   (setq TeX-parse-self t)))
-(put 'upcase-region 'disabled nil)
 
-;; Algunos modos para algunas extensiones.
+;; Varios Básicos
+(electric-pair-mode 1)
+(show-paren-mode 1)
+(setq backup-directory-alist '(("" . "~/.emacs.d/emacs-backup")))
+
+;; --- Juegos. ---
+
+(use-package tetris
+  :ensure t )
+
+(use-package typing
+  :ensure t )
+
+(use-package poker
+  :ensure t )
+
+;; --- Modos automáticos. ---
 (add-to-list 'auto-mode-alist '("\\.ens\\'" . asm-mode))
 
-;; Algunos bindings personlizados.
+;; ----- Atajos personlizados. -----
 (global-set-key (kbd "C-z") 'undo)
 (global-set-key (kbd "C-x C-k") 'kill-buffer)
 (global-set-key (kbd "C-x k") 'kill-buffer-and-window)
@@ -159,3 +176,7 @@
 (global-set-key (kbd "C-;") 'comment-line)
 (global-set-key (kbd "C-c C-f") 'set-frame-font)
 ;(global-set-key (kbd "RET") 'indent-new-comment-line)
+
+
+(put 'upcase-region 'disabled nil)
+
