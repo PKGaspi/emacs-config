7d670049dbf94eb5559a02d088d10aa5b5c81305
Juan Gaspar García <gaxdpar@gmail.com>
2020-01-04 23:00:57 +0100

    Cambiado el tema y cambios menores para mayor estabilidad.

--- init.el
+++ init.el
@@ -1,10 +1,10 @@
-; ----- Preparación. -----
+;; ----- Preparación. -----
 
-; Deshabilitar configuración custom.
+;; Deshabilitar configuración custom.
 (setq custom-file "~/.emacs.d/.emacs-custom.el")
 (setq inhibit-x-resources 't)
 
-; Conexión con melpa y melpa-stable.
+;; Conexión con melpa y melpa-stable.
 (when (>= emacs-major-version 24)
   (require 'package)
   (add-to-list
@@ -15,13 +15,13 @@
    '("melpa" . "http://melpa.org/packages/"))
   (package-initialize))
 
-; Instalación de use-package.
+;; Instalación de use-package.
 (unless (featurep 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 (eval-when-compile (require 'use-package))
 
-; --- Ajustes iniciales. ---
+;; --- Ajustes iniciales. ---
 (setq inhibit-startup-screen t)               ; Desactivar pantalla de bienvenida.
 
 (tooltip-mode -1)                             ; Desactivar barra de herramientas.
@@ -43,24 +43,23 @@
 
 ;; ----- Apariencia. -----
 
-; --- Cursor. ---
+;; --- Cursor. ---
 (setq-default cursor-type 'bar)
 
-;--- Tema. ---
-(use-package material-theme
+;; --- Tema. ---
+(use-package dracula-theme
   :ensure t
-  :init (load-theme 'material t))
-
-; --- Fondo. ---
+  :init (load-theme 'dracula t))
+;; --- Fondo. ---
 (set-background-color "#101010")
 
-; --- Fuente. ---
+;; --- Fuente. ---
 (add-to-list 'default-frame-alist '(font . "FiraCode-12"))
 (set-face-attribute 'default nil :family "FiraCode-12")
 (set-default-font "FiraCode-12")
 (load "~/.emacs.d/fira-code-mode.el") ; Usar fira-code-mode para ligaduras.
 
-; ----- Paquetes. -----
+;; ----- Paquetes. -----
 
 ; Rainbow-delimiters: paréntesis de colorines.
 (use-package rainbow-delimiters
@@ -68,7 +67,7 @@
   :init
   (add-hook 'prog-mode-hook 'rainbow-delimiters-mode 1))
 
-; Telephone-line: PowerLine en la barra inferior.
+;; Telephone-line: PowerLine en la barra inferior.
 (use-package telephone-line
   :ensure t) ;; Bueno, esto necesita un repaso gordo. Espero que mole mucho.
 (setq telephone-line-subseparator-faces '())
@@ -76,7 +75,7 @@
       telephone-line-evil-use-short-tag t)
 (telephone-line-mode t)
 
-; Windmove: moverse por las ventanas alt + flechas.
+;; Windmove: moverse por las ventanas alt + flechas.
 (use-package windmove
   :ensure t
   :bind (("M-<up>" . windmove-up)
@@ -87,7 +86,7 @@
 ;; (use-package ido
 ;;   :init (ido-mode))
 
-; Helm: mini-buffers expandidos.
+;; Helm: mini-buffers expandidos.
 ;; Thanks to @Ironjanowar for helm config.
 (use-package helm
   :ensure t
@@ -102,12 +101,12 @@
 (use-package ac-helm
   :ensure t)
 
-; Multi-term: Terminal mejorada.
+;; Multi-term: Terminal mejorada.
 ;; (use-package multi-term
 ;;   :ensure t
 ;;   :bind (("C-t" . multi-term-dedicated-toggle)))
 
-; Multiple-cursors: editar con varios cursores.
+;; Multiple-cursors: editar con varios cursores.
 (use-package multiple-cursors
   :ensure t
   :bind (("C-S-c C-S-c" . mc/edit-lines)
@@ -115,29 +114,29 @@
 	 ("C-<" . mc/mark-previous-like-this)
 	 ("C-c C-<" . mc/mark-all-like-this)))
 
-; Ace-mc: Añadir cursores fácilmente.
+;; Ace-mc: Añadir cursores fácilmente.
 (use-package ace-mc
   :ensure t)
 
-; Zone-rainbow: poner colorines a las letras.
+;; Zone-rainbow: poner colorines a las letras.
 (use-package zone-rainbow
   :ensure t)
 
-; Golden-ratio: relación aúrea en las distintas ventanas.
+;; Golden-ratio: relación aúrea en las distintas ventanas.
 (use-package golden-ratio
   :ensure t
   :config (golden-ratio-mode t))
 
-; All-the-icons: Formato para varios iconos.
+;; All-the-icons: Formato para varios iconos.
 (use-package all-the-icons
 	     :ensure t)
 
-; Neotree: explorador de archivos.
+;; Neotree: explorador de archivos.
 (use-package neotree
 	     :ensure t
 	     :bind ("C-x n" . neotree-toggle))
 
-; Undo-tree: un árbol de operaciones de deshacer/rehacer.
+;; Undo-tree: un árbol de operaciones de deshacer/rehacer.
 (use-package undo-tree
 	     :ensure t
 	     :config (global-undo-tree-mode t))
@@ -151,21 +150,21 @@
 	     :ensure t
 	     :init (yas-global-mode t))
 
-; Snippets de ejemplo.
+;; Snippets de ejemplo.
 (use-package yasnippet-snippets
   :ensure t)
 
-; Company: autocompletado.
+;; Company: autocompletado.
 (use-package company
 	     :ensure t
 	     :init (global-company-mode)
 	     :bind ("C-<tab>" . company-yasnippet))
 
-; Ac-ispell: Comprobador de ortografía (autocompletado).
+;; Ac-ispell: Comprobador de ortografía (autocompletado).
 (use-package ac-ispell
   :ensure t)
 
-; Helm-ispell: Comprobador de ortografía (helm).
+;; Helm-ispell: Comprobador de ortografía (helm).
 (use-package helm-ispell
   :ensure t)
 
